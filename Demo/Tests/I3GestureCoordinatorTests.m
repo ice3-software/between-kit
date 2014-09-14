a5013cfe730d55f5a62e097536444ff312e1b58b
stephen fortune <steve.fortune@icecb.com>
2014-09-14 23:13:06 +0100

    #28 #11 Wrote unit tests covering the coordinator's dealloc method

--- Demo/Tests/I3GestureCoordinatorTests.m
+++ Demo/Tests/I3GestureCoordinatorTests.m
@@ -12,30 +12,31 @@
 
 SpecBegin(I3GestureCoordinator)
 
+    __block id dragArena;
+    __block id superview;
+    __block id panGestureRecognizer;
+
+    beforeEach(^{
+    
+        panGestureRecognizer = OCMClassMock([UIPanGestureRecognizer class]);
+        dragArena = OCMClassMock([I3DragArena class]);
+        superview = OCMClassMock([UIView class]);
+    
+        OCMStub([dragArena superview]).andReturn(superview);
+    
+    });
+
+    afterEach(^{
+    
+        dragArena = nil;
+        panGestureRecognizer = nil;
+        superview = nil;
+    
+    });
+
 
     describe(@"constructor", ^{
 
-        __block id dragArena;
-        __block id superview;
-        __block id panGestureRecognizer;
-        
-        beforeEach(^{
-            
-            panGestureRecognizer = OCMClassMock([UIPanGestureRecognizer class]);
-            dragArena = OCMClassMock([I3DragArena class]);
-            superview = OCMClassMock([UIView class]);
-
-            OCMStub([dragArena superview]).andReturn(superview);
-            
-        });
-        
-        afterEach(^{
-            
-            dragArena = nil;
-            panGestureRecognizer = nil;
-            superview = nil;
-
-        });
         
         it(@"should inject dependencies", ^{
         
@@ -65,11 +66,54 @@ SpecBegin(I3GestureCoordinator)
 
             [[superview reject] addGestureRecognizer:[OCMArg any]];
             OCMStub([superview gestureRecognizers]).andReturn(@[panGestureRecognizer]);
-            I3GestureCoordinator* coordinator = [[I3GestureCoordinator alloc] initWithDragArena:dragArena withGestureRecognizer:panGestureRecognizer];
+            I3GestureCoordinator* coordinator __unused = [[I3GestureCoordinator alloc] initWithDragArena:dragArena withGestureRecognizer:panGestureRecognizer];
             OCMVerifyAll(superview);
-            
+       
+
+        });
+
+    });
+
+
+
+    describe(@"destructor", ^{
+
+        
+        /** @note Here we use pragma to ignore warnings about weak variables being assigned and
+            then released immediately after as this is exactly what we are trying to achieve. In
+            order for 'dealloc' to be triggered under ARC we must invoke the ctor by creating a 
+            weak reference that will unasigned immediately. */
+        
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-unsafe-retained-assign"
+        
+        
+        it(@"should unbind gesture recognizer from coordinator and superview", ^{
+ 
+            OCMStub([superview gestureRecognizers]).andReturn(@[panGestureRecognizer]);
+
+            __weak I3GestureCoordinator* coordinator __unused = [[I3GestureCoordinator alloc] initWithDragArena:dragArena withGestureRecognizer:panGestureRecognizer];
+
+            OCMVerify([panGestureRecognizer removeTarget:[OCMArg any] action:NULL]);
+            OCMVerify([superview removeGestureRecognizer:panGestureRecognizer]);
+
         });
         
+        it(@"should not attempt to remove gesture recognizer from superview if it is no long present", ^{
+            
+            [[superview reject] removeGestureRecognizer:panGestureRecognizer];
+            OCMStub([superview gestureRecognizers]).andReturn(@[]);
+            
+            __weak I3GestureCoordinator* coordinator __unused = [[I3GestureCoordinator alloc] initWithDragArena:dragArena withGestureRecognizer:panGestureRecognizer];
+
+            OCMVerifyAll(superview);
+
+        });
+
+#pragma clang diagnostic pop
+        
     });
+
+
 
 SpecEnd
