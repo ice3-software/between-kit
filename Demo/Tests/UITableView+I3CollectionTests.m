a4834000eb11236ce884bd2f0ebea534332a00c1
stephen fortune <steve.fortune@icecb.com>
2014-09-21 00:25:39 +0100

    #11 In favour of proper test driven development, wrote a failing test for the UITableView+I3Collection class category

---
+++ Demo/Tests/UITableView+I3CollectionTests.m
@@ -0,0 +1,38 @@
+//
+//  I3GestureCoordinatorTests.m
+//  BetweenKit
+//
+//  Created by Stephen Fortune on 14/09/2014.
+//  Copyright (c) 2014 stephen fortune. All rights reserved.
+//
+
+#import <BetweenKit/UITableView+I3Collection.h>
+
+
+SpecBegin(UITableViewI3Collection)
+
+
+    __block UITableView *tableViewCollection;
+
+    beforeEach(^{
+        tableViewCollection = [[UITableView alloc] initWithFrame:CGRect(0, 0, 10, 10)];
+    });
+
+    afterEach(^{
+        tableViewCollection = nil;
+    });
+
+
+    describe(@"I3Collection interface", ^{
+
+        it(@"should return a UITableView (itself) for the collectionView", ^{
+            
+            expect(tableViewCollection.collectionView).to.beInstanceOf([UITableView class]);
+            expect(tableViewCollection.collectionView).to.equal(tableViewCollection);
+            
+        });
+        
+    });
+
+
+SpecEnd
