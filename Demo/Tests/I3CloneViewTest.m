1244260da957b987a5eb02569e9d29655b6dd357
stephen fortune <steve.fortune@icecb.com>
2014-09-21 20:52:04 +0100

    #11 Wrote failing test for the clone view drawRect method

--- Demo/Tests/I3CloneViewTest.m
+++ Demo/Tests/I3CloneViewTest.m
@@ -13,18 +13,42 @@ SpecBegin(I3CloneView)
 
 
     __block I3CloneView *view;
+    __block UIView *sourceView;
+
+
+    beforeEach(^{
+    
+        sourceView = OCMPartialMock([[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)]);
+        view = [[I3CloneView alloc] initWithSourceView:sourceView];
+    
+    });
 
 
     describe(@"constructor", ^{
-
-        it(@"should setup the view with the source view", ^{
-            
-            UIView *sourceView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
-            view = [[I3CloneView alloc] initWithSourceView:sourceView];
-            
-            expect(view.sourceView).to.equal(sourceView);
-            expect(view.frame).to.equal(sourceView.frame);
         
+        
+        it(@"should setup the view with the source view", ^{
+            expect(view.sourceView).to.equal(sourceView);
+        });
+        
+        it(@"should set the clone view up with the same frame as the source view", ^{
+            expect(view.frame).to.equal(sourceView.frame);
+        });
+        
+    });
+
+
+    describe(@"drawing the clone view", ^{
+
+        it(@"should draw the source view in the cloned view on drawRect", ^{
+        
+            id layer = OCMClassMock([CALayer class]);
+            OCMStub([sourceView layer]).andReturn(layer);
+
+            [view drawRect:CGRectMake(0, 0, 10, 10)];
+            
+            OCMVerify([layer renderInContext:(__bridge CGContextRef)([OCMArg any])]);
+
         });
         
     });
