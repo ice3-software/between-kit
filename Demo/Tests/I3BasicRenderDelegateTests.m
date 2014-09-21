85ed194765e746371d267c5317fe2e8907026c96
stephen fortune <steve.fortune@icecb.com>
2014-09-21 20:01:32 +0100

    #28 #11 Began building the basic render delegate

---
+++ Demo/Tests/I3BasicRenderDelegateTests.m
@@ -0,0 +1,21 @@
+//
+//  I3GestureCoordinatorTests.m
+//  BetweenKit
+//
+//  Created by Stephen Fortune on 14/09/2014.
+//  Copyright (c) 2014 stephen fortune. All rights reserved.
+//
+
+#import <BetweenKit/I3BasicRenderDelegate.h>
+
+
+SpecBegin(I3BasicRenderDelegate)
+
+
+    describe(@"", ^{
+
+
+    });
+
+
+SpecEnd
---
+++ Pod/Classes/I3BasicRenderDelegate.h
@@ -0,0 +1,13 @@
+//
+//  I3BasicRenderDelegate.h
+//  Pods
+//
+//  Created by Stephen Fortune on 21/09/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface I3BasicRenderDelegate : NSObject
+
+@end
