d51453b132be829823e07958313739195f979c3d
stephen fortune <steve.fortune@icecb.com>
2014-09-21 20:26:58 +0100

    #28 #11 Added test for a clone view

---
+++ Demo/Tests/I3CloneViewTest.m
@@ -0,0 +1,15 @@
+//
+//  I3GestureCoordinatorTests.m
+//  BetweenKit
+//
+//  Created by Stephen Fortune on 14/09/2014.
+//  Copyright (c) 2014 stephen fortune. All rights reserved.
+//
+
+#import <BetweenKit/I3CloneView.h>
+
+
+SpecBegin(I3CloneViewTest)
+
+
+SpecEnd
