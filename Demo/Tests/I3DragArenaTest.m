4053e5417f7187ef1247e3ee2cd6d2006862570e
stephen fortune <steve.fortune@icecb.com>
2014-09-25 12:10:52 +0100

    #11 Wrote test covering the drag area ctor

---
+++ Demo/Tests/I3DragArenaTest.m
@@ -0,0 +1,49 @@
+//
+//  I3GestureCoordinatorTests.m
+//  BetweenKit
+//
+//  Created by Stephen Fortune on 14/09/2014.
+//  Copyright (c) 2014 stephen fortune. All rights reserved.
+//
+
+#import <BetweenKit/I3DragArena.h>
+
+
+SpecBegin(I3DragArena)
+
+
+
+    describe(@"constructor", ^{
+        
+        
+        it(@"should create a mutable orderable set from the elements passed in the array", ^{
+
+            NSObject *object1 = [[NSObject alloc] init];
+            NSObject *object2 = [[NSObject alloc] init];
+            NSObject *object3 = [[NSObject alloc] init];
+            
+            NSArray *elements = @[object1, object2, object3];
+            
+            I3DragArena *arena = [[I3DragArena alloc] initWithSuperview:nil containingCollections:elements];
+            
+            expect([arena.collections objectAtIndex:0]).to.equal(object1);
+            expect([arena.collections objectAtIndex:1]).to.equal(object2);
+            expect([arena.collections objectAtIndex:2]).to.equal(object3);
+            
+        });
+        
+        
+        it(@"should inject the superview", ^{
+        
+            UIView *superview = [[UIView alloc] init];
+            
+            I3DragArena *arena = [[I3DragArena alloc] initWithSuperview:superview containingCollections:nil];
+            
+            expect(arena.superview).to.equal(superview);
+        
+        });
+        
+    });
+
+
+SpecEnd
