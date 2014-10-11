4823677907b6fc437e82906c97558d51a61f497b
stephen fortune <steve.fortune@icecb.com>
2014-10-11 19:09:19 +0100

    #11 Wrote failing tests for specifying whether dragging itmes should be hidden or not in the datasource

--- Demo/Tests/I3BasicRenderDelegateTests.m
+++ Demo/Tests/I3BasicRenderDelegateTests.m
@@ -19,6 +19,7 @@ SpecBegin(I3BasicRenderDelegate)
     __block id arena;
     __block id gestureRecognizer;
     __block id currentDraggingCollection;
+    __block id currentDragDataSource;
     __block UIView *draggingItem;
     __block UIView *collectionView;
     CGPoint touchPoint = CGPointMake(10, 10);
@@ -31,6 +32,7 @@ SpecBegin(I3BasicRenderDelegate)
         arena = OCMClassMock([I3DragArena class]);
         superview = OCMPartialMock([[UIView alloc] init]);
         currentDraggingCollection = OCMProtocolMock(@protocol(I3Collection));
+        currentDragDataSource = OCMProtocolMock(@protocol(I3DragDataSource));
         draggingItem = [[UIView alloc] init];
         collectionView = [[UIView alloc] init];
         gestureRecognizer = OCMClassMock([UIPanGestureRecognizer class]);
@@ -39,6 +41,7 @@ SpecBegin(I3BasicRenderDelegate)
         OCMStub([coordinator arena]).andReturn(arena);
         OCMStub([currentDraggingCollection itemAtPoint:touchPoint]).andReturn(draggingItem);
         OCMStub([currentDraggingCollection collectionView]).andReturn(collectionView);
+        OCMStub([currentDraggingCollection dragDataSource]).andReturn(currentDragDataSource);
         OCMStub([coordinator currentDragOrigin]).andReturn(touchPoint);
         OCMStub([coordinator currentDraggingCollection]).andReturn(currentDraggingCollection);
         OCMStub([coordinator gestureRecognizer]).andReturn(gestureRecognizer);
@@ -53,29 +56,61 @@ SpecBegin(I3BasicRenderDelegate)
         currentDraggingCollection = nil;
         draggingItem = nil;
         gestureRecognizer = nil;
+        currentDragDataSource = nil;
         
     });
 
 
     describe(@"rendering", ^{
 
-        it(@"should construct a dragging view from an item in the dragging collection on start", ^{
+        
+        describe(@"begin drag", ^{
+        
             
-            CGRect convertedRect = CGRectMake(100, 100, 100, 100);
-            OCMStub([superview convertRect:draggingItem.frame fromView:collectionView]).andReturn(convertedRect);
-            
-            [renderDelegate renderDragStart:coordinator];
-            
-            expect(renderDelegate.draggingView).to.beInstanceOf([I3CloneView class]);
-            expect(renderDelegate.draggingView.sourceView).to.equal(draggingItem);
-            expect([renderDelegate.draggingView.superview isEqual:superview]).to.beTruthy;
-            expect(renderDelegate.draggingView.frame).to.equal(convertedRect);
-            
-            OCMVerify([superview convertRect:draggingItem.frame fromView:collectionView]);
+            it(@"should construct a dragging view from an item in the dragging collection on start", ^{
+                
+                CGRect convertedRect = CGRectMake(100, 100, 100, 100);
+                OCMStub([superview convertRect:draggingItem.frame fromView:collectionView]).andReturn(convertedRect);
+                
+                [renderDelegate renderDragStart:coordinator];
+                
+                expect(renderDelegate.draggingView).to.beInstanceOf([I3CloneView class]);
+                expect(renderDelegate.draggingView.sourceView).to.equal(draggingItem);
+                expect([renderDelegate.draggingView.superview isEqual:superview]).to.beTruthy;
+                expect(renderDelegate.draggingView.frame).to.equal(convertedRect);
+                
+                OCMVerify([superview convertRect:draggingItem.frame fromView:collectionView]);
+                
+            });
 
+            
+            it(@"should hide the original item if required by the datasource", ^{
+            
+                CGPoint draggingPoint = CGPointMake(10, 10);
+                OCMStub([currentDragDataSource hidesItemWhileDraggingAtPoint:draggingPoint inCollection:currentDraggingCollection]).andReturn(YES);
+                
+                [renderDelegate renderDragStart:coordinator];
+                
+                expect(draggingItem.alpha).to.equal(0.01f);
+                OCMVerify([currentDragDataSource hidesItemWhileDraggingAtPoint:draggingPoint inCollection:currentDraggingCollection]);
+                
+            });
+            
+            it(@"should not hide the original item if specified by the datasource", ^{
+                
+                CGPoint draggingPoint = CGPointMake(10, 10);
+                OCMStub([currentDragDataSource hidesItemWhileDraggingAtPoint:draggingPoint inCollection:currentDraggingCollection]).andReturn(NO);
+                
+                [renderDelegate renderDragStart:coordinator];
+                
+                expect(draggingItem.alpha).notTo.equal(0.01f);
+                OCMVerify([currentDragDataSource hidesItemWhileDraggingAtPoint:draggingPoint inCollection:currentDraggingCollection]);
+                
+            });
+            
         });
 
-        it(@"should translate the current dragging view and then reset the regognizer's translation", ^{
+        it(@"should translate the current dragging view and then reset the recognizer's translation", ^{
             
             CGPoint translation = CGPointMake(5, 5);
             [renderDelegate renderDragStart:coordinator];
@@ -107,6 +142,18 @@ SpecBegin(I3BasicRenderDelegate)
             
             expect(renderDelegate.draggingView).to.beNil();
 
+        });
+        
+        it(@"should release reference to dragging view and remove it from superview on exchange between collections", ^{
+        
+            id dstCollection = OCMProtocolMock(@protocol(I3Collection));
+            
+            [renderDelegate renderDragStart:coordinator];
+            [renderDelegate renderDropOnCollection:dstCollection atPoint:CGPointMake(0, 0) fromCoordinator:coordinator];
+            
+            expect([superview.subviews containsObject:renderDelegate.draggingView]).to.beFalsy;
+            expect(renderDelegate.draggingView).to.beNil();
+            
         });
         
     });
