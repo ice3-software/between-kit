a3dab2d68263df243c35209042b79436180a3b08
stephen fortune <steve.fortune@icecb.com>
2014-10-11 22:26:35 +0100

    #11 Added some inline comments to the rearrange test to make it bit more readable

--- Demo/Tests/I3BasicRenderDelegateTests.m
+++ Demo/Tests/I3BasicRenderDelegateTests.m
@@ -79,8 +79,12 @@ SpecBegin(I3BasicRenderDelegate)
                 
                 expect(renderDelegate.draggingView).to.beInstanceOf([I3CloneView class]);
                 expect(renderDelegate.draggingView.sourceView).to.equal(draggingItem);
-                expect([renderDelegate.draggingView.superview isEqual:superview]).to.beTruthy;
                 expect(renderDelegate.draggingView.frame).to.equal(convertedRect);
+
+                /// We can't seem to compare partial mock references, so I've just used `isEqual`... note sure
+                /// if this works.
+                
+                expect([renderDelegate.draggingView.superview isEqual:superview]).to.beTruthy;
                 
                 OCMVerify([superview convertRect:draggingItem.frame fromView:collectionView]);
                 
@@ -224,6 +228,10 @@ SpecBegin(I3BasicRenderDelegate)
                 I3CloneView *draggingView = renderDelegate.draggingView;
                 UIView *exchangeItem = [[UIView alloc] init];
                 
+                /// @note that these rect dimensions are really meaningless, that is, their only requirements is
+                /// that they are different so that OCMStub can tell the different between the different invokations
+                /// of `convertRect:fromView:`
+                
                 exchangeItem.frame = CGRectMake(1, 1, 99, 99);
                 draggingItem.frame = CGRectMake(2, 2, 88, 88);
                 
@@ -248,8 +256,17 @@ SpecBegin(I3BasicRenderDelegate)
                     
                     animateBlock();
                     
+                    /// Here we need to map the array of subviews to an array of _just_ the cloned views. We
+                    /// do this by filtering in the array using a predicate that checks against the class type
+                    /// This is so that we can then deduce the cloned exchanging view.
+                    
                     NSPredicate *clonedViewPredicate = [NSComparisonPredicate predicateWithLeftExpression:[NSExpression expressionForEvaluatedObject] rightExpression:[NSExpression expressionForConstantValue:[I3CloneView class]] customSelector:@selector(isMemberOfClass:)];
                     NSArray *clonedViews = [superview.subviews filteredArrayUsingPredicate:clonedViewPredicate];
+                    
+                    /// As we've constructed the superview, it should be guarenteed that it only has 2 I3ClonedView
+                    /// subviews. Note that in a real-world scenario this may not be the case: a user of the framework
+                    /// may add an I3ClonedView to the superview themselves, so this is not a robust assertion to
+                    /// make elsewhere.
                     
                     expect(clonedViews.count).to.equal(2);
                     
@@ -258,6 +275,10 @@ SpecBegin(I3BasicRenderDelegate)
                     
                     expect(exchangeView.frame).to.equal(draggingItemSuperRect);
                     expect(draggingView.frame).to.equal(exchangeItemSuperRect);
+                    
+                    /// We can't seem to compare partial mock references, so I've just used `isEqual`... note sure
+                    /// if this works.
+                    
                     expect([exchangeView.superview isEqual:superview]).to.beTruthy;
                     
                     completionBlock();
