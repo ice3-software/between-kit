bc3a44b8ecbe1d59701c8a566fd27f3be8fe851b
stephen fortune <steve.fortune@icecb.com>
2014-11-17 12:25:33 +0000

    #32 Added failing test for a new uitableview category

---
+++ Demo/Tests/UITableView+I3Collection.m
@@ -0,0 +1,16 @@
+//
+//  UITableView+I3Collection.m
+//  BetweenKit
+//
+//  Created by Stephen Fortune on 17/11/2014.
+//  Copyright (c) 2014 stephen fortune. All rights reserved.
+//
+
+#import <BetweenKit/UITableView+I3Collection.h>
+
+
+SpecBegin(UITableViewI3Collection)
+
+
+
+SpecEnd
