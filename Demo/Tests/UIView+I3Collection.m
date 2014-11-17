a2bfa7ef4c25d94600a6ad5e1c4e42952ae6e598
stephen fortune <steve.fortune@icecb.com>
2014-11-17 13:51:50 +0000

    #32 Wrote failing tests for simple UIView collection category

--- Demo/Tests/UIView+I3Collection.m
+++ Demo/Tests/UIView+I3Collection.m
@@ -8,3 +8,35 @@
 
 #import <BetweenKit/UIView+I3Collection.h>
 
+
+SpecBegin(UIViewI3Collection)
+
+    describe(@"I3Collection interface", ^{
+
+        it(@"should return self as the collection view", ^{
+        
+            UIView *view = [[UIView alloc] init];
+            expect(view.collectionView).to.equal(view);
+            
+        });
+        
+        it(@"should return subview at given point", ^{
+        
+            id view = OCMPartialMock([[UIView alloc] init]);
+            
+            id subview1 = OCMPartialMock([[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)]);
+            id subview2 = OCMPartialMock([[UIView alloc] initWithFrame:CGRectMake(10, 0, 10, 10)]);
+            id subview3 = OCMPartialMock([[UIView alloc] initWithFrame:CGRectMake(20, 0, 10, 10)]);
+            
+            NSArray *subviews = @[subview1, subview2, subview3];
+            OCMStub([view subviews]).andReturn(subviews);
+
+            expect([view itemAtPoint:CGPointMake(5, 5)]).to.equal(subview1);
+            expect([view itemAtPoint:CGPointMake(5, 15)]).to.equal(subview2);
+            expect([view itemAtPoint:CGPointMake(5, 25)]).to.equal(subview3);
+            
+        });
+
+    });
+
+SpecEnd
---
+++ Pod/Classes/UIView+I3Collection.m
@@ -0,0 +1,29 @@
+//
+//  UIView+I3Collection.m
+//  Pods
+//
+//  Created by Stephen Fortune on 17/11/2014.
+//
+//
+
+#import "UIView+I3Collection.h"
+
+
+@implementation UIView (I3Collection)
+
+
+#pragma mark - I3Collection implementation
+
+
+-(UIView *)collectionView{
+    return self;
+}
+
+
+-(UIView *)itemAtPoint:(CGPoint) at{
+    
+    return nil;
+}
+
+
+@end
