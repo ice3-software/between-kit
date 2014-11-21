ce6ac380c9cb9c1d44d4043cb41e160036d9f2ff
stephen fortune <steve.fortune@icecb.com>
2014-11-21 17:17:04 +0000

    #15 Changed the drag data source interface to account for appendation methods. Also added note about #36

--- Pod/Classes/I3DragDataSource.h
+++ Pod/Classes/I3DragDataSource.h
@@ -46,7 +46,7 @@
 
 /**
  
- Called to update the data source on a drop form a foreign collection.
+ Called to update the data source on an exchange of items between 2 collections.
  
  @name Coordination
  @param from            The point from the foreign draggable that the item is from.
@@ -55,7 +55,23 @@
  @param toCollection    The collection we're providing data for.
  
  */
--(void) dropItemAtPoint:(CGPoint) from fromCollection:(id<I3Collection>) fromCollection toPoint:(CGPoint) to inCollection:(id<I3Collection>) toCollection;
+-(void) exchangeItemAtPoint:(CGPoint) from inCollection:(id<I3Collection>) fromCollection withItemAtPoint:(CGPoint) to inCollection:(id<I3Collection>) toCollection;
+
+
+/**
+ 
+ Called to update the data source on the appendation of an item from one collection onto 
+ another.
+ 
+ @see `canItemAtPoint:fromCollection:beAppendedToCollection:toCollection:` for notes on semantics
+ @name Coordination
+ @param from            The point in fromCollection from which the drag originates.
+ @param to              The point in onCollection on which we drop.
+ @param fromCollection  The collection that we're dragging from.
+ @param onCollection    The collection that we're appending onto.
+ 
+ */
+-(void) appendItemAtPoint:(CGPoint) from fromCollection:(id<I3Collection>) fromCollection toPoint:(CGPoint) to onCollection:(id<I3Collection>) onCollection;
 
 
 /**
@@ -65,7 +81,7 @@
  @name Coordination
  @param from            The point from which the item is being dragged from.
  @param to              The point to which the item is being dragged to.
- @param collection      The collection we're providing data for.
+ @param collection      The collection to which the drag/drops relate.
  
  */
 -(void) rearrangeItemAtPoint:(CGPoint) from withItemAtPoint:(CGPoint) to inCollection:(id<I3Collection>) collection;
@@ -73,11 +89,11 @@
 
 /**
  
- Called to delete an item from the data source on deletion.
+ Called to delete an item from the data source.
  
  @name Coordination
- @param at              The point to which the item was.
- @param collection      The collection we're providing data for.
+ @param at              The point at which the item originates.
+ @param collection      The from which we're deleting the item.
  
  */
 -(void) deleteItemAtPoint:(CGPoint) at inCollection:(id<I3Collection>) collection;
@@ -85,23 +101,46 @@
 
 /**
  
- Returns YES or NO based on whether an item at a given point from a foreign draggable can be
- dropped on this draggable at another given point. Assumed as NO if this is not implemented.
+ Returns YES or NO based on whether an item originating at a given point from one collection 
+ can be exchanged with an item in another collection at another given point. Assumed as NO if 
+ this is not implemented.
  
  @name Coordination
- @param from            The point from the foreign draggable that the item is from.
- @param to              The point in this draggable to which the foreign item is being dragged.
- @param fromCollection  The foreign collection.
- @param collection      The collection we're providing data for.
+ @param from            The point from the original collection that the item is from
+ @param to              The point in the target collection that we're dropping on.
+ @param fromCollection  The original collection that we're dragging from.
+ @param toCollection    The target collection that we're dropping on.
  @return BOOL
  
  */
--(BOOL) canItemAtPoint:(CGPoint) from fromCollection:(id<I3Collection>) fromCollection beDroppedToPoint:(CGPoint) to inCollection:(id<I3Collection>) collection;
+-(BOOL) canItemAtPoint:(CGPoint) from fromCollection:(id<I3Collection>) fromCollection beExchangedWithItemAtPoint:(CGPoint) to inCollection:(id<I3Collection>) toCollection;
 
 
 /**
  
- Returns YES or NO based on whether an item at a given point can be exchanged in the container
+ Returns YES or NO based on whether an item at a given point from one collection can be
+ appended onto the end of another collection at another given point. Assumed as NO if this
+ is not implemented.
+
+ @note Semantically, 'append' here refers to the drag data source' implementation of
+ `appendItemAtPoint:fromCollection:toPoint:onCollection:`. Its important to note that
+ its up to the user how they want to realise appendation. E.g. the may want to implement
+ 'appending' by adding the new data at the end of a list, at the beginning of a list,
+ whereever they like really. As far as the framework's concerned, its all appendation.
+ 
+ @name Coordination
+ @param from            The point from the original collection that the item is from
+ @param to              The point in the target collection that we're dropping on.
+ @param fromCollection  The original collection that we're dragging from.
+ @param toCollection      The target collection that we're dragging to.
+ @return BOOL
+
+ */
+-(BOOL) canItemAtPoint:(CGPoint) from fromCollection:(id<I3Collection>) fromCollection beAppendedToCollection:(id<I3Collection>) toCollection atPoint: to;
+
+/**
+ 
+ Returns YES or NO based on whether an item at a given point can be rearranged in the collection
  with an item at another point. Assumed as NO if this is not implemented.
  
  @name Coordination
@@ -138,6 +177,10 @@
  @name Rendering
  @param at          The point at which the item is.
  @param collection  The collection we're providing data for.
+ @todo Remove this - it has no place here. If the basic render delegate wants to ask fine-grained 
+ questions about whether it should 'hide this' or 'style something like this', it should provide its
+ own delegate protocol. Something like `I3RenderDataSource`... Still open to discussion.
+ @deprecated
  @return BOOL
  
  */
